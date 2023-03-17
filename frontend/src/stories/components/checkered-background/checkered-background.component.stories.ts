0f1eca532b0cd6f3e01d1442a861a0307d26b3e0
tma19993 <tmazzurek@gmail.com>
2023-03-17 21:07:07 +0100

    add checkered background component to storybook

---
+++ frontend/src/stories/components/checkered-background/checkered-background.component.stories.ts
@@ -0,0 +1,37 @@
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import {DEFAULT_VIEWPORT } from "@storybook/addon-viewport";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CheckeredBackgroundModule } from './checkered-background.module';
+import { CheckeredBackgroundComponent } from './checkered-background.component';
+
+export default {
+  title: 'Core/Button',
+  decorators:[
+    moduleMetadata({
+      imports: [
+        CheckeredBackgroundModule,
+        BrowserAnimationsModule
+      ],
+    })
+  ],
+  component: CheckeredBackgroundComponent,
+  parameters:{
+    viewport: DEFAULT_VIEWPORT
+  },
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ],
+} as Meta;
+
+const Template: Story  = args => ({
+  props:{
+    ...args
+  }
+})
+
+export const Primary: Story = Template.bind({});
+Primary.args={
+}
+
---
+++ frontend/src/stories/components/checkered-background/checkered-background.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkered-background',
+  templateUrl: './checkered-background.component.html',
+  styleUrls: ['./checkered-background.component.scss']
+})
+export class CheckeredBackgroundComponent {
+
+}
