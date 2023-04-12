0ad493fab3feef7a20703a1a2ec5a8ba16e52325
tma19993 <tmazzurek@gmail.com>
2023-04-12 17:13:13 +0200

    fix button component

--- frontend/src/stories/components/footer/footer.module.ts
+++ frontend/src/stories/components/footer/footer.module.ts
@@ -1,7 +1,6 @@
 import {  NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
-import { AppButtonModule } from '../button/button.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { AppButtonModule } from '../button/button.module';
   ],
   imports: [
     CommonModule,
-    AppButtonModule
   ],
 })
 export class FooterModule { }
