20cf6813cc7d91e475dd6371200ab78832f72d82
tma19993 <tmazzurek@gmail.com>
2023-04-17 18:17:56 +0200

    add translation with asstets data

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClientModule} from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
+import { FooterModule } from 'src/stories/components/footer/footer.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FooterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot(
+      {
+        loader:{
+          provide: TranslateLoader,
+          useFactory: (handler: HttpClient) => { return new TranslateHttpLoader(handler, './assets/i18n/', '.json');},
+          deps:[HttpClient]
+        }
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/stories/components/footer/footer.module.ts
+++ frontend/src/stories/components/footer/footer.module.ts
@@ -1,6 +1,9 @@
 import {  NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,16 @@ import { FooterComponent } from './footer.component';
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
+    TranslateModule.forRoot(
+      {
+        loader:{
+          provide: TranslateLoader,
+          useFactory: (handler: HttpClient) => { return new TranslateHttpLoader(handler, './assets/i18n/', '.json');},
+          deps:[HttpClient]
+        }
+      }
+    )
   ],
 })
 export class FooterModule { }
